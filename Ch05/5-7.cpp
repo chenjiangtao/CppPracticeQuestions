3b41b65aaa720f1e6afc52d794fea57e5339a8ab
wangsiping97 <wangsiping1997@gmail.com>
2019-01-28 01:20:44 +0800

    修复了重复创建对象的bug

--- Ch05/5-7.cpp
+++ Ch05/5-7.cpp
@@ -10,9 +10,11 @@ class Cat {
 public: 
     Cat(int n): n(n) {
         numOfCats++;
+        cout<<"Cat constructor..."<<n<<endl;
     }
     ~Cat() {
         numOfCats--;
+        cout<<"Cat destructor..."<<n<<endl;
     }
     static int getNumOfCats() { // 静态成员函数
         return  numOfCats; // 'static' can only be specified inside the class definition
@@ -29,12 +31,35 @@ int main()
     Cat *c[5];
     for (int i = 0; i < 5; i++) {
         c[i] = new Cat(i);
-        cout<<Cat(i).getNumOfCats()<<" "; // 2 3 4 5 6
+        cout<<"numOfCats = "<<c[i]->getNumOfCats()<<endl; 
     }
     cout<<endl;
     for (int i = 4; i >= 0; i--) {
         delete c[i];
-        cout<<Cat :: getNumOfCats()<<" "; // 4 3 2 1 0
+        cout<<"numOfCats = "<<Cat :: getNumOfCats()<<endl; 
     }
     return 0;
 }
+
+// 输出
+// Cat constructor...0
+// numOfCats = 1
+// Cat constructor...1
+// numOfCats = 2
+// Cat constructor...2
+// numOfCats = 3
+// Cat constructor...3
+// numOfCats = 4
+// Cat constructor...4
+// numOfCats = 5
+
+// Cat destructor...4
+// numOfCats = 4
+// Cat destructor...3
+// numOfCats = 3
+// Cat destructor...2
+// numOfCats = 2
+// Cat destructor...1
+// numOfCats = 1
+// Cat destructor...0
+// numOfCats = 0
